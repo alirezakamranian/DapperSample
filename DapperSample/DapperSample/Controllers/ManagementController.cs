a1aa2d9b603a2cea99ee7331463329f6d094f608
AlirezaKamranian <123934502+alirezakamranian@users.noreply.github.com>
2024-10-11 23:35:27 +0330

    Feat:Add more management actions

--- DapperSample/DapperSample/Controllers/ManagementController.cs
+++ DapperSample/DapperSample/Controllers/ManagementController.cs
@@ -3,6 +3,8 @@ using DapperSample.DataAccess;
 using DapperSample.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using System.Xml.Linq;
 
 namespace DapperSample.Controllers
 {
@@ -12,22 +14,46 @@ namespace DapperSample.Controllers
     {
         private readonly IDataContext _context = context;
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllUsers()
-        {
-            var users = await _context.Db
-                .QueryAsync<User>("SELECT * FROM Users;");
 
-            return Ok(users);
-        }
-
-        [HttpGet("search")]
-        public async Task<IActionResult> GetAllUsers(string chars)
+        [HttpGet("searchusers")]
+        public async Task<IActionResult> SearchInUsers(string chars)
         {
             var users = await _context.Db
                 .QueryAsync<User>($"SELECT * FROM Users WHERE Name LIKE '%{chars}%';");
 
             return Ok(users);
+        }
+
+        [HttpGet("authorscount")]
+        public async Task<IActionResult> GetAuthorsCount()
+        {
+            var parameters = new { IsAuthor = true };
+            var users = await _context.Db
+                .QueryAsync($"SELECT Count(Id) As AuthorsCount FROM Users WHERE IsAuthor = @IsAuthor", parameters);
+
+            return Ok(users);
+        }
+
+        [HttpGet("usersarticles")]
+        public async Task<IActionResult> GetUsersArticlesCount(int minimumNumber = 0)
+        {
+            var parameters = new { MinimumNumber = minimumNumber };
+
+            var articlesCount = await _context.Db
+                .QueryAsync($"SELECT COUNT(AuthorArticles.ArticleId) AS ArticleCount,AuthorId FROM AuthorArticles,Authors GROUP BY AuthorId HAVING COUNT(AuthorArticles.ArticleId) >= @MinimumNumber",parameters);
+
+            return Ok(articlesCount);
+        }
+
+        [HttpGet("authordetail")]
+        public async Task<IActionResult> GetUsersArticlesCount(string authorId )
+        {
+            var parameters = new { AuthorId = authorId };
+
+            var articlesCount = await _context.Db
+                .QueryAsync($"SELECT Authors.*,Users.Name,Users.Phone FROM Users INNER JOIN Authors ON Authors.UserId = Users.Id WHERE Authors.Id = @AuthorId;", parameters);
+
+            return Ok(articlesCount);
         }
     }
 }
